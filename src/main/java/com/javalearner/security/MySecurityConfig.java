1fd2c1ad4be67ca55d817ed3fd6399e0d8441330
a-java-learner-2022 <a.java.learner.2022@gmail.com>
2023-06-22 14:19:21 -0400

    allowing /users to admin_user & /public to normal_user.

--- src/main/java/com/javalearner/security/MySecurityConfig.java
+++ src/main/java/com/javalearner/security/MySecurityConfig.java
@@ -15,7 +15,8 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers("/public/**").permitAll()
+			.antMatchers("/public/**").hasRole("NORMAL")
+			.antMatchers("/users/**").hasRole("ADMIN")
 			.anyRequest()
 			.authenticated()
 			.and()
